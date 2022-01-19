8f6e8415e52d4318f91b30af9a543f0c3093d60a
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-01-18 21:17:50 -0700

    added users to schema table

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,24 @@
 DROP DATABASE IF EXISTS todos_db;
 CREATE DATABASE todos_db;
 
+
 USE todos_db;
+
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id);
+);
 
 CREATE TABLE todos (
     id INT NOT NULL AUTO_INCREMENT,
     task VARCHAR(30) NOT NULL,
     completed BOOLEAN DEFAULT false,
     date_created DATETIME DEFAULT CURRENT_TIMESTAMP, 
-    PRIMARY KEY (id)
+    userId INT NOT NULL,
+    PRIMARY KEY (id), 
+    FOREIGN KEY (userID)
+    REFERENCES users(id)
+    ON DELETE SET NULL
 );
