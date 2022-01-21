010fc0a568569b085c64b4808476337d1da79c5b
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-01-20 20:42:23 -0700

    created POST, PATCH, and DELETE request

--- server.js
+++ server.js
@@ -32,14 +32,20 @@ app.post('/api/users', async (req, res) => {
     }
 })
 
-
-
-
+app.get('/api/users', async (req, res) => {
+    try {
+        const getAllUserQuery= 'SELECT * FROM todos;';
+        const [users]= await connection.query(getAllUsersQuery);
+        res.json(users);
+    }catch (e) {
+        res.status(400).json(e);
+    }
+});
 
 
 app.get('/api/todos', async (req, res) => {
     try {
-        const getAllTodosQuery= 'SELECT * FROM todos;';
+        const getAllTodosQuery= 'SELECT todos.id, task, completed, userId, username FROM todos LEFT JOIN users ON todos.userId=users.id';
         const [todos]= await connection.query(getAllTodosQuery);
         res.json(todos);
     }catch (e) {
@@ -53,7 +59,7 @@ app.get('/api/todos', async (req, res) => {
 
 app.post('/api/todos', async (req, res) => {
 
-const {task} = req.body;
+const {task, userId} = req.body;
 // If the user does not provide a task, respond with an error
 if(!task){
     return res.status(400).json({error: 'You must provide a task'});
@@ -63,9 +69,9 @@ if(!task){
 // if any lines of the code throws an error, JS will take that error and 
 // put that error in the "catch" block, and then run the code in the "catch block"
 try {
-    const insertQuery = 'INSERT INTO todos(task) VALUES(?);';
+    const insertQuery = 'INSERT INTO todos(task, userId) VALUES(?, ?);';
     const getTodoById = 'SELECT * FROM todos WHERE id = ?;';
-    const [result] = await connection.query(insertQuery, [task]);
+    const [result] = await connection.query(insertQuery, [task, userId,]);
     //whenever we do an INSERT, UPDATE, OR DELETE query in mysql2 or mysql npm package
     // it doesn't give us the data that was interacted with. it instead tells us information about the
     //about how many rows were affected and maybe the inserted id or updated of the regarding data
@@ -76,7 +82,40 @@ try {
 }catch(e){
     res.status(400).json(e);
 }
+});
 
+app.patch('/api/todos/:todoId', async (req, res) => {
+const{todoId}= req.params;
+const {task, completed}= req.body;
+if(!task || !completed){
+    return res.status(400).json({error: 'you must provide the task and completed'});
+}
+try {
+    const updateTodoById = 'UPDATE todos SET task = ?, completed = ? WHERE id = ?';
+    const getTodoById= 'SELECT * FROM todos WHERE id =?;'
+    const isCompleted = completed.toLowerCase() === 'true' ? 1 : 0;
+    await connection.query(updateTodoById, [task, isCompleted, todoId]);
+    const [todos]= await connection.query(getTodoById, todoId);
+    res.json(todos[0]);
+} catch (e) {
+    res.status(400).json(e);
+}
+});
+
+app.delete('/api/todos/:todoId', async (req, res) => {
+    const {todoId}= req.params;
+    try {
+        const getTodoById= 'SELECT * FROM todos WHERE id =?;';
+        const delelteTodoById ='DELETE FROM todos WHERE id=?;';
+        const [todos]= await connection.query(getTodoById, todoId);
+        if(!todos[0]){
+            return res.status(400).json({error: 'Todo not found'});
+        }
+        await connection.query(delelteTodoById, todoId);
+        res.json(todos[0]);
+    } catch (e) {
+        res.status(400).json(e);
+    }
 });
 
 app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
